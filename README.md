8eb62a8f0f20e2b453108ae0901aebd01f96abb1
ITJacob <jacob.wd8816@gmail.com>
2021-10-21 11:37:01 +0800

    feat: add pixi.js

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# typescript-qkqwfm
+# typescript-pixi-v5
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-qkqwfm)
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-pixi-v5)
+[View on StackBlitz ⚡️](https://typescript-pixi-v5.stackblitz.io)
--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
+import * as PIXI from 'pixi.js';
 // Import stylesheets
 import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+// Create the application helper and add its render target to the page
+let app = new PIXI.Application({ width: 640, height: 360 });
+document.body.appendChild(app.view);
+
+const graphics = new PIXI.Graphics();
+graphics.beginFill(0xff3300);
+
+graphics.drawRect(0, 0, 100, 200);
+graphics.endFill();
+app.stage.addChild(graphics);
+
+// Add a ticker callback to move the sprite back and forth
+let elapsed = 0.0;
+app.ticker.add((delta) => {
+  elapsed += delta;
+  graphics.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
+});
